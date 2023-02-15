f841b3a68f136d9cc8e980ce008feb53703ac6b3
Geetha Shambu <geetha.shambu@gmail.com>
2023-02-15 18:46:56 +0530

    Modified changes on cart page

--- cypress/e2e/testScripts/VerifyCartpage.cy.js
+++ cypress/e2e/testScripts/VerifyCartpage.cy.js
@@ -2,37 +2,34 @@
 import Demo from "../../PageObjectModel/Sauce"
 import Home from "../../PageObjectModel/Homepage"
 import Cart from "../../PageObjectModel/Cartpage"
+
 describe("new application",()=>{
 
-  })
+})
 
-    beforeEach(function(){
+  beforeEach(function(){
 
-     cy.visit("https://www.saucedemo.com/")
-      
-    })
-   it('Login page',()=>{
-      cy.fixture("Sauce").then(function(data){
-         this.Logindata=data
-         const Login=new Demo();
-         const Ln=new Home();
-         const Shop=new Cart();
+   cy.visit("https://www.saucedemo.com/")
     
-         Login.Setuser().type(this.Logindata.username)
-         Login.Setpassword().type(this.Logindata.password)
-         Login.Submit().click()
-         //Ln.Verifyhomepage().should('have.text',"SwagLabs")
-         Ln.Productone().click()
-         Ln.Product2().click()
-         Ln.Cart().click()
-        //verify cartpage
-         Shop.VerifyCart().should('have.text','Your Cart') 
-         
-         
+  })
+ it('Login page',()=>{
+    cy.fixture("Sauce").then(function(data){
+       this.Logindata=data
+       const Login=new Demo();
+       const ln=new Home();
+         const shop=new Cart();
+  
+       Login.Setuser().type(this.Logindata.username)
+       Login.Setpassword().type(this.Logindata.password)
+       Login.Submit().click()
+       //Ln.Verifyhomepage().should('have.text',"SwagLabs")
+       ln.Productone().click()
+       ln.Product2().click()
+       ln.Cart().click()
+      //verify cartpage
+       shop.VerifyCart().should('have.text','Your Cart') 
 
-        
-      })
-
+       
     })
 
-  
+  })
